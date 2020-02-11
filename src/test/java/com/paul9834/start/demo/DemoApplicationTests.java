7b623f736eb61d68beaae42d4301a019cd6c55e8
Paul9834 <kmontealegremelo@gmail.com>
2020-02-11 15:53:32 -0500

    Initial commit

---
+++ src/test/java/com/paul9834/start/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul9834.start.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
